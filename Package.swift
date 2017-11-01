e461a35f12c10e62b5096d77efa0a7a0df9cba24
bannzai <kingkong999yhirose@gmail.com>
2017-11-01 23:35:33 +0900

    :memo: define package

--- Package.swift
+++ Package.swift
@@ -13,16 +13,21 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/bannzai/XcodeProject.git", from: Version(0, 1, 1)),
+        .package(url: "https://github.com/behrang/YamlSwift.git", from: Version(3, 4, 0)),
+        .package(url: "https://github.com/kareman/SwiftShell.git", from: Version(4, 0, 0))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Kuri",
-            dependencies: []),
+            dependencies: ["KuriCore"]),
+        .target(
+            name: "KuriCore",
+            dependencies: ["XcodeProject", "YamlSwift", "SwiftShell"]),
         .testTarget(
-            name: "KuriTests",
-            dependencies: ["Kuri"]),
+            name: "KuriCoreTests",
+            dependencies: ["KuriCore"]),
     ]
 )
