53783b0dd39693c303c4ff46d4ede2ea2213ec6e
bannzai <kingkong999yhirose@gmail.com>
2018-01-02 21:03:01 +0900

    :metal: add demo project

---
+++ KuriDemo/KuriDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KuriDemo
+//
+//  Created by Yudai.Hirose on 2018/01/02.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
