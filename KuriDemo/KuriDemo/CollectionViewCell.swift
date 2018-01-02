829370a89daabbcc772c792e2eb2261b20ceaa72
bannzai <kingkong999yhirose@gmail.com>
2018-01-02 21:06:34 +0900

    :metal: add cells

---
+++ KuriDemo/KuriDemo/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  KuriDemo
+//
+//  Created by Yudai.Hirose on 2018/01/02.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ KuriDemo/KuriDemo/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  KuriDemo
+//
+//  Created by Yudai.Hirose on 2018/01/02.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
