41a682666e496ad277b436c3f4781c728dea3c17
bannzai <kingkong999yhirose@gmail.com>
2018-08-07 16:46:07 +0900

    :sparkles: Overwrite test template

--- KuriDemo/templates/KuriTemplate/Tests/__PREFIX__Tests.swift
+++ KuriDemo/templates/KuriTemplate/Tests/__PREFIX__Tests.swift
@@ -6,30 +6,31 @@
 //  Copyright © __YEAR__ __USERNAME__. All rights reserved.
 //
 
-import UIKit
+import XCTest
+@testable import KuriDemo
 
-protocol __PREFIX__Builder {
-    func build() -> UIViewController
-}
-
-
-struct __PREFIX__BuilderImpl: __PREFIX__Builder {
-    func build() -> UIViewController {
-        let viewController = __PREFIX__ViewController()
-        viewController.inject(
-            presenter: __PREFIX__PresenterImpl(
-                view: viewController,
-                wireframe: __PREFIX__WireframeImpl(
-                    viewController: viewController
-                ),
-                useCase: __PREFIX__UseCaseImpl(
-                    repository: __PREFIX__RepositoryImpl (
-                        dataStore: __PREFIX__DataStoreImpl()
-                    ),
-                    translator: __PREFIX__TranslatorImpl()
-                )
-            )
-        )
-        return viewController
+class __PREFIX__Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
 }
