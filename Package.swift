9df58c1175b1ffc2dc7c544f20970c8f758f2661
bannzai <kingkong999yhirose@gmail.com>
2019-12-02 01:32:47 +0900

    :memo: Missing package DSL

--- Package.swift
+++ Package.swift
@@ -7,9 +7,10 @@ let package = Package(
     name: "Kuri",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(name: "kuri", targets: ["KuriCore"]),
         .library(
-            name: "Kuri",
-            targets: ["Kuri"]),
+            name: "KuriCore",
+            targets: ["KuriCore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
