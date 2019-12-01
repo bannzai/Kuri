d8ad6b77d4853521d653fdd03efc6cd5ff3c6d66
bannzai <kingkong999yhirose@gmail.com>
2019-12-02 01:22:40 +0900

    :up: Dependencies

--- Package.swift
+++ Package.swift
@@ -14,9 +14,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/bannzai/XcodeProject.git", from: Version(0, 1, 5)),
-        .package(url: "https://github.com/behrang/YamlSwift.git", from: Version(3, 4, 0)),
-        .package(url: "https://github.com/kareman/SwiftShell.git", from: Version(4, 0, 0)),
-        .package(url: "https://github.com/kylef/Commander.git", from: Version(0, 8, 0))
+        .package(url: "https://github.com/behrang/YamlSwift.git", from: Version(3, 4, 4)),
+        .package(url: "https://github.com/kareman/SwiftShell.git", from: Version(5, 0, 1)),
+        .package(url: "https://github.com/kylef/Commander.git", from: Version(0, 9, 1))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
